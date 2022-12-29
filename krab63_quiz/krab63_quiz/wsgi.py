7be84cc7a4b890a0b39b24035c50b45bd038b1bb
Anton Milov <milovanton@gmail.com>
2022-12-29 19:36:15 +0300

    Readme added

--- krab63_quiz/krab63_quiz/wsgi.py
+++ krab63_quiz/krab63_quiz/wsgi.py
@@ -11,27 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# Если в папаке проекта есть файл 'settings_local.py', то будут использованы
-# его настройки, в противном случае - настройки из 'settings.py'
-
-# file_name = 'settings_local.py'
-# cur_dir = os.getcwd()
-
-# file_list = os.listdir(cur_dir)
-# if file_name in file_list:
-#     os.environ['DJANGO_SETTINGS_MODULE'] = 'krab63_quiz.settings_local'
-# else:
-#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'krab63_quiz.settings')
-
-# dirname = os.path.dirname(__file__)
-# relative_path = os.path.join(dirname, 'settings_local.py')
-# settings_local_exists = os.path.exists(relative_path)
-
-# if settings_local_exists:
-#     os.environ['DJANGO_SETTINGS_MODULE'] = 'krab63_quiz.settings_local'
-# else:
-#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'krab63_quiz.settings')
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'krab63_quiz.settings')
 
 application = get_wsgi_application()
